3503d6e77ed0a46b6aad3c2a2d8f7fb743d9a4b3
badnumbers <badnumbersmusic@gmail.com>
2025-06-04 19:19:30 +0100

    Finally got the config crap sorted.

--- ArabicVocabBuddy.Client/Program.cs
+++ ArabicVocabBuddy.Client/Program.cs
@@ -1,23 +1,31 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ArabicVocabBuddy.Client;
+using System.Text.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 // dotnet run sets `builder.HostEnvironment.Environment` to "Development" by default
-// to get the App Service to set `builder.HostEnvironment.Environment` to "Production", in the Azure Portal, set the environment variable:
-/// ASPNETCORE_ENVIRONMENT=Production            
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
-                     .AddJsonFile($"appsettings.{builder.HostEnvironment.Environment}.json", optional: true, reloadOnChange: false);
+// Otherwise it's whatever the environment variable ASPNETCORE_ENVIRONMENT is set to.
+// And I guess that defaults to "Production" if not set, as it's not currently set in the app service I'm using
 
-//builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]) });
+var appSettingsFilename = builder.HostEnvironment.IsDevelopment() ? "appsettings.Development.json" : "appsettings.json";
+builder.Configuration.AddJsonFile(appSettingsFilename, optional: false, reloadOnChange: false); 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string? apiBaseUrl;
+using (var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+{
+    var configJson = await http.GetStringAsync(appSettingsFilename);
+    using var doc = JsonDocument.Parse(configJson);
+    apiBaseUrl = doc.RootElement.GetProperty("ApiBaseUrl").GetString();
+}
+
 builder.Services.AddScoped<IEnvironmentManager>(sp =>
 {
     var config = sp.GetRequiredService<IConfiguration>();
-    return new ArabicVocabBuddy.Client.EnvironmentManager(config, builder.HostEnvironment.Environment);
+    return new EnvironmentManager(config, builder.HostEnvironment.Environment);
 });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl!) });
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
